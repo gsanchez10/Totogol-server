a857a1bf77c58f8bdc805bca8ca13eb0ea51ae76
Gabriel Sanchez <gabrielsanchez@Gabriels-MacBook-Pro.local>
2017-09-19 07:45:24 -0600

    agrega configuracion para que el servidor sirva el index.html cuando entra una solicitud que no es al api

--- index.js
+++ index.js
@@ -7,15 +7,20 @@ const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 // DB Setup
 mongoose.connect('mongodb://localhost:totogol/totogol');
 
 // App Setup
+app.use(express.static('../client'));
 app.use(morgan('combined'));//logging, for debugging
 app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
+app.get('*', function response(req, res) {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
 
 // Server Setup
 const port = process.env.PORT || 3090;
